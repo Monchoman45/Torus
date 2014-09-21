ef3747e755f187bbf8e65e869703c0d659b99a9e
Monchoman45 <monchoman45@gmail.com>
2014-09-20 22:30:10 -0400

    join message

--- commands.js
+++ commands.js
@@ -1,30 +1,33 @@
 Torus.commands = {};
 Torus.commands.join = {
-	help: 'Join a room. Accepts either the id of the room, or the domain name (for example, community will take you to the room for [[w:|community.wikia.com]]). Specifying 0 will part all rooms.',
+	help: 'Join a room. Accepts either the id of the room, or the domain name (for example, community will take you to the room for [[w:|community.wikia.com]]). Specifying 0 will part all rooms.', //FIXME: i18n
 	func: function(room) {
-		if(room == '0') {Torus.logout();}
-		if(!Torus.database[room]) {return 'Unable to look up ' + room + ' in database. Try [[w:c:' + room + ':Special:Torus]].';}
+		if(room == '0') {
+			Torus.logout();
+			return;
+		}
+		if(!Torus.database[room]) {return 'Unable to look up ' + room + ' in database. Try [[w:c:' + room + ':Special:Torus]], or [https://github.com/Monchoman45/Torus/issues/new?labels=database-addition ask to get it added to the database].\n';} //FIXME: i18n
 
 		(new Torus.classes.Chat(Torus.database[room].room, room)).connect();
 	}
 };
 Torus.commands.part = {
-	help: 'Leave a room. If no room is specified, the current room is left.',
+	help: 'Leave a room. If no room is specified, the current room is left.', //FIXME: i18n
 	func: function(room) {
 		if(!room) {Torus.ui.active.disconnect('closed');}
 		else {
-			if(!Torus.chats[room]) {return 'Invalid room ' + room + '.';}
+			if(!Torus.chats[room]) {return 'Invalid room ' + room + '.';} //FIXME: i18n
 			else {Torus.chats[room].disconnect('closed');}
 		}
 	}
 };
 Torus.commands.quit = '/logout';
 Torus.commands.logout = {
-	help: 'Leave every room.',
+	help: 'Leave every room.', //FIXME: i18n
 	func: Torus.logout
 };
 Torus.commands.kick = {
-	help: 'Kicks a user from the current room.',
+	help: 'Kicks a user from the current room.', //FIXME: i18n
 	func: function() {
 		var user = '';
 		for(var i = 0; i < arguments.length; i++) {user += ' ' + arguments[i];}
@@ -33,7 +36,7 @@ Torus.commands.kick = {
 	}
 };
 Torus.commands.ban = {
-	help: 'Bans or rebans a user from the current room.',
+	help: 'Bans or rebans a user from the current room.', //FIXME: i18n
 	func: function(expiry) {
 		var user = '';
 		for(var i = 1; i < arguments.length; i++) {user += ' ' + arguments[i];}
@@ -42,7 +45,7 @@ Torus.commands.ban = {
 	}
 };
 Torus.commands.unban = {
-	help: 'Unbans a user from the current room.',
+	help: 'Unbans a user from the current room.', //FIXME: i18n
 	func: function() {
 		var user = '';
 		for(var i = 0; i < arguments.length; i++) {user += ' ' + arguments[i];}
@@ -52,7 +55,7 @@ Torus.commands.unban = {
 };
 Torus.commands.mod = '/givemod';
 Torus.commands.givemod = {
-	help: 'Promotes a user to chatmod in the current room.',
+	help: 'Promotes a user to chatmod in the current room.', //FIXME: i18n
 	func: function() {
 		var user = '';
 		for(var i = 0; i < arguments.length; i++) {user += ' ' + arguments[i];}
@@ -63,7 +66,7 @@ Torus.commands.givemod = {
 Torus.commands.query = '/private';
 Torus.commands.priv = '/private';
 Torus.commands.private = {
-	help: 'Opens a private room. Users can be specified in a comma-separated list.',
+	help: 'Opens a private room. Users can be specified in a comma-separated list.', //FIXME: i18n
 	func: function() {
 		var users = '';
 		for(var i = 0; i < arguments.length; i++) {users += ' ' + arguments[i];}
@@ -72,7 +75,7 @@ Torus.commands.private = {
 	}
 };
 Torus.commands.away = {
-	help: 'Toggles your away status for the current room.',
+	help: 'Toggles your away status for the current room.', //FIXME: i18n
 	func: function() {
 		var message = '';
 		for(var i = 0; i < arguments.length; i++) {message += ' ' + arguments[i];}
@@ -87,14 +90,14 @@ Torus.commands.away = {
 	}
 };
 Torus.commands.back = {
-	help: 'Sets your status as present for the current room.',
+	help: 'Sets your status as present for the current room.', //FIXME: i18n
 	func: function(message) {
 		if(!message) {message = '';}
 		Torus.ui.active.set_status('here', message);
 	}
 };
 Torus.commands.status = {
-	help: 'Changes your status state or message for the current room.',
+	help: 'Changes your status state or message for the current room.', //FIXME: i18n
 	func: function(state) {
 		var message = '';
 		for(var i = 1; i < arguments.length; i++) {message += ' ' + arguments[i];}
@@ -111,7 +114,7 @@ Torus.commands.status = {
 };*/
 Torus.commands.db = '/database';
 Torus.commands.database = {
-	help: 'Look up domains and room ids in the database.',
+	help: 'Look up domains and room ids in the database.', //FIXME: i18n
 	func: function(room) {
 		if(!room) { //print everything
 			var str = '';
@@ -124,17 +127,15 @@ Torus.commands.database = {
 	}
 };
 Torus.commands.options = {
-	help: 'View options.',
-	func: function() {
-		Torus.ui.activate(-1);
-	}
+	help: 'View options.', //FIXME: i18n
+	func: function() {Torus.ui.activate(Torus.ext.options);}
 };
 Torus.commands.fullscreen = {
-	help: 'Make Torus fullscreen.',
+	help: 'Make Torus fullscreen.', //FIXME: i18n
 	func: Torus.ui.fullscreen
 };
 Torus.commands.help = {
-	help: 'Displays help data.',
+	help: 'Displays help data.', //FIXME: i18n
 	func: function() {
 		var str = '';
 		for(var i = 0; i < arguments.length; i++) {str += ' ' + arguments[i];}
@@ -142,8 +143,8 @@ Torus.commands.help = {
 
 		if(str) {
 			var help = Torus.commands.eval(str, 'help');
-			if(!help) {Torus.alert('No help data for ' + str);}
-			else {return 'Help: ' + str + ': ' + help;}
+			if(!help) {Torus.alert('No help data for ' + str);} //FIXME: i18n
+			else {return 'Help: ' + str + ': ' + help;} //FIXME: i18n
 		}
 		else {
 			var coms = '';
@@ -151,7 +152,7 @@ Torus.commands.help = {
 				if(typeof Torus.commands[i] != 'function' && typeof Torus.commands[i] != 'string') {coms += ', ' + i;}
 			}
 			coms = coms.substring(2);
-			return 'Commands:\n' + coms;
+			return 'Commands:\n' + coms; //FIXME: i18n
 		}
 	}
 };
